c9845451f31729a36047d31640614f27cb10cf14
Michał Kubiak <michal.kubiak@my.com>
2020-11-24 08:50:20 +0100

    Init commit

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  plugins: [
+    'gatsby-plugin-typescript',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /\.svg$/,
+        },
+      },
+    },
+    'gatsby-plugin-react-helmet-async',
+  ],
+};
