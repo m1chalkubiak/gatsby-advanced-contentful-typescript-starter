799e4e5156bf43ee8363f954199447a73b4b2467
Michał Kubiak <michal.kubiak@my.com>
2021-02-02 12:46:14 +0100

    Add Contentful config

--- src/shared/components/users/users.stories.tsx
+++ src/shared/components/users/users.stories.tsx
@@ -1,6 +1,10 @@
 import React, { ComponentProps } from 'react';
 import { Story, Meta } from '@storybook/react';
+import { times } from 'ramda';
 
+import { withRedux } from '../../utils/storybook';
+import { userFactory } from '../../../mocks/factories';
+import { prepareState } from '../../../mocks/store';
 import { Users } from '.';
 
 const Template: Story<ComponentProps<typeof Users>> = () => <Users />;
@@ -10,4 +14,11 @@ export const Default = Template.bind({});
 export default {
   title: 'Users',
   component: Users,
+  decorators: [
+    withRedux(
+      prepareState((state) => {
+        state.users.users = times(() => userFactory(), 10);
+      })
+    ),
+  ],
 } as Meta;
